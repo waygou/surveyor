505defdb8423e746402feb8e7264507724e1b691
Waygou <bruno.falcao@waygou.com>
2018-12-07 20:59:46 +0100

    wip

--- src/Abstracts/SurveyorModel.php
+++ src/Abstracts/SurveyorModel.php
@@ -2,12 +2,14 @@
 
 namespace Waygou\Surveyor\Abstracts;
 
+use Hyn\Tenancy\Traits\UsesTenantConnection;
 use Waygou\Helpers\Traits\CanSaveMany;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 abstract class SurveyorModel extends Model
 {
+    use UsesTenantConnection;
     use SoftDeletes;
     use CanSaveMany;
 
